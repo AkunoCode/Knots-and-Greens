14046c1c0c8cb25f7138fae320b8d184a97dc1e0
John Leo Echevaria <echevariajohnleo@gmail.com>
2023-06-11 07:38:45 +0800

    Fixed the responsive position of header texts

--- Frontend/knots-and-greens/src/Components/Home.js
+++ Frontend/knots-and-greens/src/Components/Home.js
@@ -7,11 +7,12 @@ function App() {
       <NavBar />
       <div id='Header-Image'>
         <img src='./Media/Header_Pic.png' alt='Knots and Greens' />
+      </div>
+      <div id='Header-Texts'>
         <h1 id='Title'>Something handmade is so much more meaningful</h1>
         <p><i>Knot</i> your average style. Discover the art of macramé</p>
         <button id='ShopNow-Button'>Shop Now</button>
       </div>
-
     </>
   );
 }
