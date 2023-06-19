1bea671b32fb41d03480afc4d06e841e76aca17e
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-06-19 22:15:46 +0800

    Placed the useState in parent component (App.js)

--- Frontend/knots-and-greens/src/Components/App.js
+++ Frontend/knots-and-greens/src/Components/App.js
@@ -5,18 +5,21 @@ import MainPage from './MainPage/MainPage';
 import Admin from './Admin/Admin';
 import SignUp from './LoginSignup/SignUp';
 import Login from './LoginSignup/Login';
+import { useState } from 'react';
 
 function App() {
     // Routes
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<MainPage />}>
-                    <Route index element={<Home />} />
-                    <Route path='Shop' element={<Shop />} />
-                    <Route path='Admin' element={<Admin />} />
-                    <Route path='Login' element={<Login />} />
-                    <Route path="Signup" element={<SignUp />} />
+                <Route path='/' element={<MainPage props={{ isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }} />}>
+                    <Route index element={<Home props={{ isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }} />} />
+                    <Route path='Shop' element={<Shop props={{ isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }} />} />
+                    <Route path='Admin' element={<Admin props={{ isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }} />} />
+                    <Route path='Login' element={<Login props={{ isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }} />} />
+                    <Route path="Signup" element={<SignUp props={{ isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }} />} />
                     <Route path='*' element={<h1 id='NotFound'>404 Not Found</h1>} />
                 </Route>
             </Routes>
