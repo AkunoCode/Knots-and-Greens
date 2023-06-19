3fbeb3cab2fd368ff4a85b529184f9795337d228
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-06-19 12:46:50 +0800

    connected the login API to the frontend

--- Frontend/knots-and-greens/src/Components/LoginSignup/Login.js
+++ Frontend/knots-and-greens/src/Components/LoginSignup/Login.js
@@ -11,31 +11,44 @@ function Login() {
     const [customers, setCustomers] = useState([]);
     const [isLoggedIn, setIsLoggedIn] = useState(true);
     const [isAdmin, setIsAdmin] = useState(false);
+    const [failedLogin, setFailedLogin] = useState(false);
 
     // Loading Data on first load of the page
     useEffect(() => {
-        getAccounts();
-    }, [])
+        setFailedLogin(false);
+    }, [email, password])
 
-    // Loading Data from the database and setting the state variable products
-    const getAccounts = async () => { // Asynchronous function
-        try {
-            const response = await axios.get(URL_PATH) // await for the response from the server
-            setCustomers(response.data.result)
-        } catch (error) {
-            alert('An Error Occured While Loading the Contents')
-        }
-    }
+    // // Loading Data from the database and setting the state variable products
+    // const getAccounts = async () => { // Asynchronous function
+    //     try {
+    //         const response = await axios.get(URL_PATH) // await for the response from the server
+    //         setCustomers(response.data.result)
+    //     } catch (error) {
+    //         alert('An Error Occured While Loading the Contents')
+    //     }
+    // }
 
     // Check if email and password matches the database
     const handleSubmit = (event) => {
-        const foundCustomer = customers.find((customer) => customer.email === email && customer.password === password)
-        if (foundCustomer) {
-            setIsLoggedIn(true)
-            clearInputs();
-        } else {
-            alert('Invalid Email or Password')
+        event.preventDefault();
+        const payload = {
+            email: email,
+            password: password
         }
+        const response = axios.post(URL_PATH + '/login', payload).then((response) => {
+            if (response.data.message === "Successfully logged in.") {
+                setIsLoggedIn(true);
+                setFailedLogin(false);
+                clearInputs();
+            }
+        }
+        ).catch((error) => {
+            if (error.response.status === 401) {
+                setFailedLogin(true);
+            } else {
+                alert('An Error Occured While Logging In')
+            }
+        });
     }
 
     //Clears the inputs
@@ -54,6 +67,7 @@ function Login() {
                 <p>Password</p>
                 <input type="password" placeholder="sample123" value={password} onChange={(e) => { setPassword(e.target.value) }} id='Password-Input' />
                 <button onClick={handleSubmit} id='Login-Button'>Login</button>
+                {failedLogin ? <p id='Failed-Login'>Invalid Email or Password</p> : null}
             </div>
         </div>
     )
