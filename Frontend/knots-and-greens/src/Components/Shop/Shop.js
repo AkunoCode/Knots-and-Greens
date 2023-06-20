f0a2a46dff5194aa3208c8bb5eb4664ab1c4d7b8
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-06-20 22:04:40 +0800

    Dispalying the cart on the side of the store

--- Frontend/knots-and-greens/src/Components/Shop/Shop.js
+++ Frontend/knots-and-greens/src/Components/Shop/Shop.js
@@ -8,16 +8,20 @@ const URL_PATH = 'http://localhost:2003/products'
 
 function Shop({ props }) {
     //Props
-    const { isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin } = props;
+    const { isLoggedIn } = props;
 
     // State Variables
     const [products, setProducts] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
+    const [cart, setCart] = useState([]);
 
-    // Loading Data on first load of the page
+    // Loading Data if there is a change in the cart or login state
     useEffect(() => {
         loadData();
-    })
+        if (isLoggedIn) {
+            getCart();
+        }
+    }, [isLoggedIn, cart])
 
     // Loading Data from the database and setting the state variable products
     const loadData = async () => {
@@ -30,6 +34,27 @@ function Shop({ props }) {
             alert('An Error Occured While Loading the Contents')
         }
     }
+
+    // Getting the cart of the user
+    const getCart = async () => {
+        try {
+            const response = await axios.get(`http://localhost:2003/carts`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+
+            })
+            // if the cart is empty or no-cart, set the cart to an empty array
+            if (response.data.message === "No Cart") {
+                setCart([])
+            } else {
+                setCart(response.data.result)
+            }
+        } catch (error) {
+            alert('An Error Occured While Loading the Contents')
+        }
+    }
+
 
     // SEARCH FUNCTION
     const [search, setSearch] = useState("");
@@ -64,8 +89,8 @@ function Shop({ props }) {
                         <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Searching for an item?" id="SearchBar" />
                         <div id="ResultsContainer">
                             {matchProducts.length !== 0 ?
-                                matchProducts.map((match) => {
-                                    return match !== null ? <ProductStack item={match} props={isLoggedIn} /> : null
+                                matchProducts.map((match, index) => {
+                                    return match !== null ? <ProductStack item={match} props={isLoggedIn} key={index} /> : null
                                 })
                                 : NoMatch = false}
                         </div>
@@ -73,6 +98,9 @@ function Shop({ props }) {
                     </div>
                     <div id="Cart-Section">
                         <h2>Your Cart</h2>
+                        {isLoggedIn && cart.length !== 0 ? cart.products.map((item) => {
+                            return <p>{item.productName} = {item.price}</p>
+                        }) : ""}
                         {!isLoggedIn ?
                             <p id="NotLoggedIn">You are not logged in. Please login to view your cart.</p>
                             : <button id="CheckOut">Check Out Now</button>}
